592a5a89383805be1418d04460e07e3d2237982d
Andrey Miroshnikov <andrei_mir@mail.ru>
2020-03-08 18:59:01 +0300

    navbar

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,30 +1,13 @@
 <template>
-<v-app>
-    <navbar>
-    </navbar>
-      
-    <v-toolbar dense>
-      <v-app-bar-nav-icon @click="drawer= !drawer" ></v-app-bar-nav-icon>
-      <v-toolbar-title>Programmer's notes</v-toolbar-title>
-      <v-spacer></v-spacer>      
-    </v-toolbar>
-    <v-navigation-drawer v-model="drawer">
-    <v-item-group>
-      <v-item>database</v-item>
-      <v-spacer></v-spacer>
-      <v-item>docker</v-item>
-    </v-item-group>
-    </v-navigation-drawer>
+  <v-app>
+    <Navbar></Navbar>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
-  data () {
-    return {
-      drawer: false,
-      s: 0
-    }
-  }
-}
+  components: { Navbar }
+};
 </script>
---
+++ hello-world/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <v-toolbar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Programmer's notes</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>mdi-docker</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content right>
+              <v-list-tile-title class="black--text font-weight-bold">Docker</v-list-tile-title>                    
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false      
+    };
+  }
+};
+</script>
